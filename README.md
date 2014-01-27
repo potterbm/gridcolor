8d3be164930bb1431c5830cf92e19d7b88403cb0
Bryan Potter <potterbm@gmail.com>
2014-01-26 21:38:15 -0700

    Sketched out the readme

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 gridcolor
 =========
 
-jQuery plugin to color tiles in a grid
+The gridcolor plugin is a jQuery plugin to color tiles in a grid. It accepts any number of colors and attempts to use each of them fairly without having two adjacent colors be the same.
+
+## Usage
+
+The gridcolor plugin is fairly easy to use. All it requires is a list of colors and a class that tells it which elements to color.
+
+The simplest example would work with HTML like this:
+
+```html
+<div class=".item-container">
+	<div class="item">Item 1</div>
+	<div class="item">Item 2</div>
+	<div class="item">Item 3</div>
+	<div class="item">Item 4</div>
+	<div class="item">Item 5</div>
+</div>
+```
+
+This javascript would then make each element with the `.item` be either red, yellow, or blue.
+
+```javascript
+$(".item-container").gridcolor({
+	itemSelector : ".item",
+	colors : ["#ff0000", "#00ff00", "#0000ff"]
+});
+```
+
+## Configuration
+
+### Options
+
+### The list of colors
+
+The list of colors
+
+
+## Utility functions
+
+### `hexToRGB`
+
+The gridcolor plugin includes a utility function to convert from hex color values to RGB notation. The function is called `hexToRGB` and can be used as follows:
+
+```javascript
+$.gridcolor.hexToRGB("#000000"); // Returns "rgb(0, 0, 0)"
+```
--- jquery-gridcolor.js
+++ jquery-gridcolor.js
@@ -1,144 +1,99 @@
-
-
-var gridLayoutDefaults = {
-	columns : 3,
-	container : ".item-container",
-	itemSelector : ".item",
-	gutter : 30
-}
-
-function gridLayout(options)
-{
-	options = $.extend({}, gridLayoutDefaults, options);
-	
-	options["columns"] = parseInt(options["columns"]);
-	options["gutter"] = parseInt(options["gutter"]);
-	
-	$items = $(options["container"]).children(options["itemSelector"]).css("margin", 0);
-	
-	var columnHeights = [];
-	var xOffset = parseInt($items.first().outerWidth()) + options.gutter;
-	
-	
-	for(var c = 0; c < options.columns; c++) {
-		columnHeights[c] = 0;
-	}
-	
-	$items.each(function(index, element) {
+(function($) {
+	$.fn.gridcolor = function(options) {
+		console.log(options.colors);
+		// Sanitize options
+		var settings = $.extend({}, $.fn.gridcolor.defaults, options);
+		settings.columns = parseInt(settings.columns);
 		
-		// Find which column to put this item in
-		var column = keyOfLowest(columnHeights);
-		
-		// Position the item in the column
-		$(element).css("position", "absolute");
-		$(element).css("top", toCSSMeasurement(columnHeights[column]));
-		$(element).css("left", toCSSMeasurement(xOffset * column));
-		
-		// Update column heights array
-		columnHeights[column] += $(element).outerHeight() + options.gutter;
-	});
-}
-
-
-function keyOfLowest(array) {
-	return array.indexOf(Math.min.apply(Math, array));
-}
-
-
-function toCSSMeasurement(value) {
-	if(typeof(value) == "number") {
-		return value + "px";
-	}
-	else {
-		return value;
-	}
-}
-
-
-
-var colorGridDefaults = {
-	colors : ["#777777", "#999999", "#bbbbbb"],
-	columns : 3,
-	container : ".item-container",
-	cssProperty : "background-color",
-	itemSelector : ".item"
-}
-
-function colorGrid(options)
-{
-	options = $.extend({}, colorGridDefaults, options);
-	
-	options["columns"] = parseInt(options["columns"]);
-	for(var h = 0; h < options.colors.length; h++) {
-		if(options.colors[h].charAt(0) == '#') {
-			options.colors[h] = hexToRGB(options.colors[h].slice(1));
-		}
-	}
-	
-	$items = $(options.container).children(options.itemSelector);
-	
-	$items.each(function(index, element) {
-		
-		var color;
-		for(var c = 0; c < options.colors.length; c++) {
-			// Pick a color from the list
-			color = options.colors[c];
-			console.log("considering color " + color + " for element " + index);
-			
-			// Check to see if that color can be used.
-			console.log("left: " + $items.eq(index - 1).css(options.cssProperty));
-			console.log("above: " + $items.eq(index - options.columns).css(options.cssProperty));
-			if($items.eq(index - 1).css(options.cssProperty) == color || $items.eq(index - options.columns).css(options.cssProperty) == color) {
-				continue;
+		console.log(settings.colors);
+		for(var h = 0; h < settings.colors.length; h++) {
+			if(typeof(settings.colors[h] != "object")) {
+				
+				// Convert each string to an object that can be passed to $.css();
+				if(typeof(settings.colors[h]) != "string") {
+					settings.colors[h] = settings.defaultColor;
+				}
+				else if(settings.colors[h].charAt(0) == '#') {
+					settings.colors[h] = {};
+					settings.colors[h][settings.cssProperty] = hexToRGB(settings.colors[h].slice(1));
+				}
+				else {
+					settings.colors[h] = {};
+					settings.colors[h][settings.cssProperty] = settings.colors[h];
+				}
 			}
 			else {
-				// If it works, use it
-				$(element).css(options.cssProperty, color);
-				console.log("using color " + color + " for element " + index);
-				options.colors.push(options.colors.shift());
-				return true;
+				
+				// Standardize all object values to rgb(red, green, blue) format
+				for(prop in settings.colors[h]) {
+					if(settings.colors[h].hasOwnProperty(prop) && settings.colors[h][prop].charAt(0) == '#') {
+						settings.colors[h][prop] = hexToRGB(settings.colors[h][prop].slice(1));
+					}
+				}
 			}
+			
 		}
 		
-		// If no color works, use a random selection
-		color = options.colors[Math.floor(Math.random() * options.colors.length)];
-		console.log("random selection: " + color);
-		$(element).css(options.cssProperty, color);
-		options.colors.push(options.colors.shift());
-	});
-}
-
-
-function hexToRGB(hex) {
-	
-    var bigint = parseInt(hex, 16);
-    var r = (bigint >> 16) & 255;
-    var g = (bigint >> 8) & 255;
-    var b = bigint & 255;
-    
-    console.log(hex + " => " + "rgb(" + r + ", " + g + ", " + b + ")");
-    
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-}
-
-
-
-$(document).ready(function(e) {
-	var colors = ["#14A697", "#F2C12E", "#F29D35", "#F25252"];
-	var colors2 = ["#00AAB5", "#C9E000", "#ED4200", "eeeeee"];
-	
-	gridLayout({
-		container : "#content-wrapper",
-		columns : 3,
-		itemSelector : ".entry",
-		gutter : 30
-	});
-	
-	colorGrid({
-		colors : colors2,
-		columns : 3,
-		container : "#content-wrapper",
-		cssProperty : "background-color",
-		itemSelector : ".entry"
-	})
-});
+		console.log(settings.colors);
+		
+		return this.each(function() {
+			$items = $(this).children(settings.itemSelector);
+			
+			$items.each(function(index, element) {
+				
+				var color;
+				
+				for(var c = 0; c < settings.colors.length; c++) {
+					
+					// Pick a color from the list
+					color = settings.colors[c];
+					
+					// Check to see if that color can be used
+					if($items.eq(index - 1).css(settings.cssProperty) == color[settings.cssProperty] || $items.eq(index - settings.columns).css(settings.cssProperty) == color[settings.cssProperty]) {
+						continue;
+					}
+					else {
+						// If it works, use it
+						$(element).css(color);
+						
+						// Rotate the list of colors
+						settings.colors.push(settings.colors.shift());
+					}
+				}
+				
+				// If no color works, use a random selection
+				color = settings.colors[Math.floor(Math.random() * settings.colors.length)];
+				$(element).css(color);
+				
+				// Rotate the list of colors
+				settings.colors.push(settings.colors.shift());
+				
+			});
+		});
+		
+		$.fn.gridcolor.defaults = {
+			colors : ["#777777", "#999999", "#bbbbbb", "#555555"],
+			columns : 3,
+			cssProperty : "background-color",
+			defaultColor : 
+				{
+					"background-color" : "rgb(255, 255, 255)",
+					"color" : "rgb(85, 85, 85)"
+				},
+			itemSelector : ".item"
+		};
+		
+		$.fn.gridcolor.hexToRGB = function(hex) {
+				
+    		var bigint = parseInt(hex, 16);
+    		var r = (bigint >> 16) & 255;
+    		var g = (bigint >> 8) & 255;
+    		var b = bigint & 255;
+		    
+    		console.log(hex + " => " + "rgb(" + r + ", " + g + ", " + b + ")");
+		    
+    		return "rgb(" + r + ", " + g + ", " + b + ")";
+		};
+		
+	};
+}(jQuery));
