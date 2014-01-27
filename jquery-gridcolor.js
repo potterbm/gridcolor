9baf7aaf7bce7ce013ea45af396d359a9c93dce4
Bryan Potter <potterbm@gmail.com>
2014-01-26 22:01:49 -0700

    It works! ...Sort of

--- jquery-gridcolor.js
+++ jquery-gridcolor.js
@@ -1,13 +1,18 @@
 (function($) {
 	$.fn.gridcolor = function(options) {
-		console.log(options.colors);
+		
 		// Sanitize options
 		var settings = $.extend({}, $.fn.gridcolor.defaults, options);
 		settings.columns = parseInt(settings.columns);
 		
-		console.log(settings.colors);
 		for(var h = 0; h < settings.colors.length; h++) {
-			if(typeof(settings.colors[h] != "object")) {
+			console.log(h);
+			console.log(settings.colors[h]);
+			console.log(typeof(settings.colors[h]));
+			console.log(typeof(settings.colors[h]) == "object");
+			console.log(typeof(settings.colors[h]) != "object");
+			if(typeof(settings.colors[h]) != "object") {
+				console.log('not object');
 				
 				// Convert each string to an object that can be passed to $.css();
 				if(typeof(settings.colors[h]) != "string") {
@@ -15,7 +20,7 @@
 				}
 				else if(settings.colors[h].charAt(0) == '#') {
 					settings.colors[h] = {};
-					settings.colors[h][settings.cssProperty] = hexToRGB(settings.colors[h].slice(1));
+					settings.colors[h][settings.cssProperty] = $.fn.gridcolor.hexToRGB(settings.colors[h].slice(1));
 				}
 				else {
 					settings.colors[h] = {};
@@ -27,14 +32,13 @@
 				// Standardize all object values to rgb(red, green, blue) format
 				for(prop in settings.colors[h]) {
 					if(settings.colors[h].hasOwnProperty(prop) && settings.colors[h][prop].charAt(0) == '#') {
-						settings.colors[h][prop] = hexToRGB(settings.colors[h][prop].slice(1));
+						settings.colors[h][prop] = $.fn.gridcolor.hexToRGB(settings.colors[h][prop].slice(1));
 					}
 				}
 			}
 			
 		}
 		
-		console.log(settings.colors);
 		
 		return this.each(function() {
 			$items = $(this).children(settings.itemSelector);
@@ -71,29 +75,30 @@
 			});
 		});
 		
-		$.fn.gridcolor.defaults = {
-			colors : ["#777777", "#999999", "#bbbbbb", "#555555"],
-			columns : 3,
-			cssProperty : "background-color",
-			defaultColor : 
-				{
-					"background-color" : "rgb(255, 255, 255)",
-					"color" : "rgb(85, 85, 85)"
-				},
-			itemSelector : ".item"
-		};
-		
-		$.fn.gridcolor.hexToRGB = function(hex) {
-				
-    		var bigint = parseInt(hex, 16);
-    		var r = (bigint >> 16) & 255;
-    		var g = (bigint >> 8) & 255;
-    		var b = bigint & 255;
-		    
-    		console.log(hex + " => " + "rgb(" + r + ", " + g + ", " + b + ")");
-		    
-    		return "rgb(" + r + ", " + g + ", " + b + ")";
-		};
-		
+	};
+	
+	
+	$.fn.gridcolor.defaults = {
+		colors : ["#777777", "#999999", "#bbbbbb", "#555555"],
+		columns : 3,
+		cssProperty : "background-color",
+		defaultColor : 
+			{
+				"background-color" : "rgb(255, 255, 255)",
+				"color" : "rgb(85, 85, 85)"
+			},
+		itemSelector : ".item"
+	};
+	
+	$.fn.gridcolor.hexToRGB = function(hex) {
+			
+   		var bigint = parseInt(hex, 16);
+   		var r = (bigint >> 16) & 255;
+   		var g = (bigint >> 8) & 255;
+   		var b = bigint & 255;
+	    
+   		console.log(hex + " => " + "rgb(" + r + ", " + g + ", " + b + ")");
+	    
+   		return "rgb(" + r + ", " + g + ", " + b + ")";
 	};
 }(jQuery));
